95e6a2f1cfa650d8f7e3f2f24613659e3018b582
Bruno Martins <bgcngm@gmail.com>
2023-10-25 14:55:35 +0100

    odroidc4_tab: Add proper skeleton
    This way EPPE works!
    
    Change-Id: I5407d3b1033f6b6c7e2f849098d7d2b45a2d5e20

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_odroidc4_tab.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_odroidc4_tab-eng \
+    lineage_odroidc4_tab-user \
+    lineage_odroidc4_tab-userdebug
